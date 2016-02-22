2265ace33ee2dec39c880786fc826fd4f8aee5d9
Cain.Chu <cainmaila@gmail.com>
2016-02-22 17:49:24 +0800

    dev

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
 var gulp = require('gulp');
 var webpack = require('webpack-stream');
 var gulpLivereload = require('gulp-livereload');
+var gulpUglify = require('gulp-uglify');
 gulp.task('scripts', function() {
 	return gulp.src('./xxx.js') //隨便打
 		.pipe(webpack(require('./webpack.config.js')))
+		.pipe(gulpUglify())
 		.pipe(gulp.dest('www/js/'))
-		.pipe(gulpLivereload()); 
+		.pipe(gulpLivereload());
 });
 gulp.task('watch', function() {
 	gulpLivereload.listen();
